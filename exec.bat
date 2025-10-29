b0e22c25aafdb7359b327fc94f460d94488780b6
k-z-w-e-b-3-1-0 <56874780+k-z-w-e-b-3-1-0@users.noreply.github.com>
2025-10-29 19:45:30 +0900

    Add exec.bat to execute PowerShell script

---
+++ exec.bat
@@ -0,0 +1,9 @@
+cd %~dp0
+
+@echo off
+
+set NOW=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+echo %NOW%
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File run.ps1 "column_name" " condition" > "query%NOW%.sql"
+
+pause
